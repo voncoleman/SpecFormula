0792d8ae044f7943f12ceaefd0ade01e4d454712
Von Coleman <von.coleman@yahoo.com>
2023-10-12 22:35:00 -0500

    first commit

---
+++ src/SpecFormula.java
@@ -0,0 +1,5 @@
+public class SpecFormula { //Last one for tonight. Keep pressing on. Be brilliant at the basics. Don't get discouraged! Ask for help! YOU GOT THIS!
+    public static void main(String[] args) {
+        System.out.println( 4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11)));
+    }
+}
